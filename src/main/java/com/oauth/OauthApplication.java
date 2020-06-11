19a1a2a9b81b880f247e97ccbda1c5ba1667f6d8
Administrator <2032218443@qq.com>
2020-06-12 02:13:00 +0800

    Initial commit

---
+++ src/main/java/com/oauth/OauthApplication.java
@@ -0,0 +1,14 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+}
